d70ed0e5f2e0b1a422f8a8cbfcc8af3be4565f99
Simran <mdrsimran@gmail.com>
2020-10-27 19:07:03 -0500

    commit tree 2a44d1a1cdb3c02af1618b40076a0627a4958d6e

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 def hopper
 	programmer_hash = 
  		{
@@ -15,6 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
+     return programmer_hash[:grace_hopper]
 
 
 end
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+    #  binding.pry
+
+     return programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -79,8 +84,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+# binding.pry
+programmer_hash[:yukihiro_matsumoto] = {
+      known_for: "Ruby", 
+      languages: ["LISP", "C"]
+}
+    return programmer_hash
 end
 
 def changing_alan
@@ -101,6 +110,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for]=alans_new_info
+
+     return programmer_hash
      
      
 end
@@ -121,5 +133,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages]<< "Assembly"
+
+     return programmer_hash
 
 end
